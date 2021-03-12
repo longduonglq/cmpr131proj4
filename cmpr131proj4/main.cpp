682f6e26d44f3eb18cf2c575415fcaa7e3b769b3
Christopher <programmerchristopher@gmail.com>
2021-03-11 16:27:34 -0800

    main & rational

--- cmpr131proj4/main.cpp
+++ cmpr131proj4/main.cpp
@@ -1,20 +1,32 @@
-// cmpr131proj4.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Name: 
+// Date: 
+// Description: 
 
 #include <iostream>
+#include "rationals/rational.h"
 
-int main()
-{
-    std::cout << "Hello World!\n";
+//test
+int main() {
+	using namespace rat;
+	Rational R1;
+	Rational R2(4, 5);
+	Rational R3(13);
+
+	cin >> R1;
+	cin >> R2;
+
+	cout << "\nR1 = " << R1 << "\n";
+	cout << "R2 = " << R2 << "\n";
+	cout << "R3 = " << R3 << "\n";
+
+	cout << "R1 == R2 -> " << (R1 == R2) << "\n";
+	cout << "R1 > R2 -> " << (R1 > R2) << "\n";
+	cout << "R1 >= R2 -> " << (R1 >= R2) << "\n";
+	cout << "R1 < R2 -> " << (R1 < R2) << "\n";
+	cout << "R1 <= R2 -> " << (R1 <= R2) << "\n";
+
+	cout << "R1 + R2 = " << (R1 + R2) << "\n";
+	cout << "R1 - R2 = " << (R1 - R2) << "\n";
+	cout << "R1 * R2 = " << (R1 * R2) << "\n";
+	cout << "R1 / R2 = " << (R1 / R2) << "\n";
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
--- cmpr131proj4/rationals/rational.h
+++ cmpr131proj4/rationals/rational.h
@@ -1,1 +1,36 @@
-#pragma once
+// Name: 
+// Date: 
+// Description: 
+
+#ifndef RATIONAL_H
+
+#define RATIONAL_H
+#include <iostream>
+using namespace std;
+
+namespace rat {
+	class Rational {
+	private:
+		void normalize();
+	public:
+		int numer;
+		int denom;
+		Rational();
+		Rational(int numer, int denom);
+		Rational(int numer);
+
+		friend istream& operator >> (istream& ins, Rational& object);
+		friend ostream& operator << (ostream& outs, const Rational& object);
+
+		friend bool operator == (const Rational obj1, const Rational& obj2);
+		friend bool operator < (const Rational obj1, const Rational& obj2);
+		friend bool operator <= (const Rational obj1, const Rational& obj2);
+		friend bool operator > (const Rational obj1, const Rational& obj2);
+		friend bool operator >= (const Rational obj1, const Rational& obj2);
+		friend Rational operator + (const Rational obj1, const Rational& obj2);
+		friend Rational operator - (const Rational obj1, const Rational& obj2);
+		friend Rational operator * (const Rational obj1, const Rational& obj2);
+		friend Rational operator / (const Rational obj1, const Rational& obj2);
+	};
+}
+#endif
