bc58b738950b1b42fa2bfeeaf6a26431a0595808
Duyen Tran <hongduyen.16697@gmail.com>
2021-03-14 00:38:51 -0800

    DuyenTRan

--- cmpr131proj4/polynomials/driver.h
+++ cmpr131proj4/polynomials/driver.h
@@ -1,5 +1,9 @@
 #pragma once
 #include "polynomials.h"
+#include<vector>
+#include<iostream>
+
+using namespace std;
 
 // get polynomials. first ask for degree then ask user to input each coefficients
 Polynomials getPolynomials();
@@ -16,5 +20,14 @@ void polyMain();
 //DuyenTran
 Polynomials getPolynomials()
 {
+	int degree;
+	cout << "Enter the degree of the polynomial: ";
+	cin >> degree;
+
+	for (int i = 0, i <= degree; i++)
+	{
+		cout << "Enter the coefficient for term #" << i << ": ";
+		coefficients.push_back();
+	}
 
 }
